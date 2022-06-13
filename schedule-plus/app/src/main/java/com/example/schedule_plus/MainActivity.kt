a6a1e6b641afba1b78d2e46d82bce3215ecd0680
Inderway <47097657+Inderway@users.noreply.github.com>
2022-06-13 19:36:05 +0800

    add activity

--- schedule-plus/app/src/main/java/com/example/schedule_plus/MainActivity.kt
+++ schedule-plus/app/src/main/java/com/example/schedule_plus/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.schedule_plus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val buttonSchedule: Button =findViewById(R.id.button_schedule)
+        buttonSchedule.setOnClickListener{
+            val intent=Intent(this, Schedule::class.java)
+            startActivity(intent)
+        }
     }
 }
